213fdc6d17f678537d9f6fe5a648a2f2b82cb115
Ravindra Gupta <ravindra.gupta@capgemini.com>
2023-08-16 23:38:00 +0530

    Automation fixes

--- ReproducerApp/src/automationHelper.js
+++ ReproducerApp/src/automationHelper.js
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
 
 export default function getAutomationID(automationID) {
-  return Platform.OS === 'ios' ? { testID: automationID } : { accessibilityLabel: automationID };
+  return Platform.OS === 'ios' ? { testID: automationID, accessibilityLabel: automationID } : { accessibilityLabel: automationID };
 }
